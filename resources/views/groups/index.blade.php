87aadb1efad3e9911286abdce7958f580c66abe5
JaySurve <Jaysurve.2004@gmail.com>
2025-02-25 14:42:39 +0530

    1 remove

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -24,7 +24,7 @@
         @foreach ($groups as $group)
             <div class="bg-blue-100 p-4 rounded-lg border border-gray-800 shadow-md hover:bg-gradient-to-b from-blue-100 to-teal-500 transform hover:scale-105 transition duration-150">
                 <div class="text-center">
-                    <h3 class="text-l font-bold mb-2">{{ $group->name }} 1</h3>
+                    <h3 class="text-l font-bold mb-2">{{ $group->name }}</h3>
                     <p class="text-gray-600 mb-2"><strong>Group ID:</strong> {{ $group->group_id }}</p> 
                     <div class="flex justify-center space-x-2 mt-4">
                     <button onclick="showGroupDetails({{ $group->id }})" class="bg-blue-500 text-white py-1 px-2 rounded hover:bg-blue-700">View</button>
