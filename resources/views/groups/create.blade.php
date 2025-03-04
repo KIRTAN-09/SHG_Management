57bd1abd8e631092ff98d93dffcc4300885403f9
JaySurve <Jaysurve.2004@gmail.com>
2025-03-04 13:00:01 +0530

    remove Group Secretary Name,Group President Name, no of members from group table

--- resources/views/groups/create.blade.php
+++ resources/views/groups/create.blade.php
@@ -101,17 +101,13 @@ input[type="submit"]:hover {
             <label for="village_name">Village Name</label>
             <input type="text" name="village_name" class="form-control" required>
         </div>
-            <div class="form-group">
-                <label for="president_name">Group President Name</label>
-                <input type="text" name="president_name" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="secretary_name">Group Secretary Name</label>
-                <input type="text" name="secretary_name" class="form-control" required>
-            </div>
         <!-- <div class="form-group">
-            <label for="no_of_members">No. of Members</label>
-            <input type="number" name="no_of_members" class="form-control" required>
+            <label for="president_name">Group President Name</label>
+            <input type="text" name="president_name" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="secretary_name">Group Secretary Name</label>
+            <input type="text" name="secretary_name" class="form-control" required>
         </div> -->
         <button type="submit" class="btn btn-primary">Add Group</button>
     </form>
