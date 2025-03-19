e7e16614fec7e141b076827b8b2cd8d375f59e50
kishor-9 <sutharkishor2012@gmail.com>
2025-03-19 11:41:08 +0530

    Remove unused DataTables CSS links and add a 'Create New Saving' button in savings index view

--- resources/views/savings/index.blade.php
+++ resources/views/savings/index.blade.php
@@ -2,11 +2,7 @@
 
 @section('content')
 <link rel="stylesheet" href="{{ asset('css/table.css') }}">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/select/1.3.3/css/select.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/datetime/1.1.0/css/dataTables.dateTime.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap5.min.css">
+
 <br>
 <div class="mb-3">
     <a href="{{ route('savings.create') }}" class="btn btn-primary">Create New Saving</a>
